eb0a2cf6d5ae11185f64b69a286d13a345e5d752
Attila Horvath <sweeptic@gmail.com>
2025-05-05 12:54:06 +0200

    10. Adding Images in Next JS

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Image from 'next/image';
+import homeImg from '@/public/home.jpg';
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <div className="absolute -z-10 inset-0">
+        <Image alt="home" src={homeImg} fill style={{ objectFit: 'cover' }} />
+      </div>
+    </div>
+  );
 }
--- app/performance/page.tsx
+++ app/performance/page.tsx
@@ -1,3 +1,11 @@
+import Image from 'next/image';
+import performanceImg from '@/public/performance.jpg';
+
 export default function Home() {
-  return <div>Performance Page </div>;
+  return (
+    <div>
+      Performance Page
+      <Image alt="performance" src={performanceImg} />
+    </div>
+  );
 }
