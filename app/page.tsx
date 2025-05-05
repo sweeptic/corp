8ad1624bbaf50f71684956a962876a0cab5066cd
Attila Horvath <sweeptic@gmail.com>
2025-05-05 12:08:19 +0200

    05. Linking Between Pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div>Home Page </div>;
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href={'/performance'}>Go To Performance</Link>
+          </li>
+          <li>
+            <Link href={'/reliability'}>Go To Reliability</Link>
+          </li>
+          <li>
+            <Link href={'/scale'}>Go To Scale</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
